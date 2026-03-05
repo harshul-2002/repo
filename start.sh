d51e0625283415434c0859f663df8e0a77bfa939
Harsh kumar <harshkumarharsh109@gmail.com>
2026-03-05 17:28:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/keycloak/keycloak/releases/download/24.0.4/keycloak-24.0.4.tar.gz -o keycloak.tar.gz
-tar -xzf keycloak.tar.gz
+echo "Installing Java..."
+apt-get update
+apt-get install -y openjdk-17-jdk
 
+echo "Downloading Keycloak..."
+curl -L https://github.com/keycloak/keycloak/releases/download/24.0.4/keycloak-24.0.4.tar.gz -o keycloak.tar.gz
+
+tar -xzf keycloak.tar.gz
 cd keycloak-24.0.4
 
+echo "Starting Keycloak..."
 bin/kc.sh start-dev --http-port=$PORT --hostname-strict=false
